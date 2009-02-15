e16c48089c33ef088ab5050252c0b071d69485e3
Jon-Anders <mail@jteigen.com>
2009-02-15 20:50:18 +0100

    added test file and small variation in output

--- src/main/scala/scalademo/Demo.scala
+++ src/main/scala/scalademo/Demo.scala
@@ -1,16 +1,26 @@
 package scalademo
 
 import scala.collection.mutable.ArrayBuffer
-/*
 
+/* Copyright [yyyy] [name of copyright owner]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 trait Demo {
   private[this] case class Demoable(description: String, run: () => Any)
   case class Demonstration(name: String, index: Int)
 
   private[this] val demonstrations = new ArrayBuffer[Demoable]
-
-  private[this] def pretty(description: String) = "[" + description + "]"
 
   private[this] def width = (0 /: demonstrations)((biggest, demoable) => biggest max demoable.description.length)
 
@@ -24,11 +34,15 @@ trait Demo {
   private[this] def demonstrate(demonstration: Demoable) {
     display(demonstration)
     println(demonstration.run())
+    dashes()
+  }
+
+  private [this] def dashes(){
     println("-" * (width + 3 + demonstrations.length.toString.length))
   }
 
   private[this] def display(demonstration: Demoable) {
-    println(demonstrations.indexOf(demonstration) + " " + pretty(demonstration.description))
+    println("[" + demonstrations.indexOf(demonstration) + " " + demonstration.description + "]")
   }
 
   def demo(description: String)(f: => Any) {
@@ -41,6 +55,7 @@ trait Demo {
 
   def list(criteria: Demonstration => Boolean) {
     findMatching(criteria).foreach(display)
+    dashes()
   }
 
   def startsWith(names: List[String]) =
---
+++ src/test/scala/scalademo/DemoTest.scala
@@ -0,0 +1,41 @@
+package scalademo
+
+/* Copyright [yyyy] [name of copyright owner]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+object DemoTest extends Application with Demo {
+  demo("simple"){
+    "a simple demo"
+  }
+
+  demo("addition 2 + 2"){
+    2 + 2
+  }
+
+  demo("multiplication 2 * 2"){
+    2 * 2
+  }
+
+  println("# list all")
+  list(_ => true)
+
+  println("# run first")
+  run(_.index == 0)
+
+  println("# list all with name containing 2")
+  list(_.name contains "2")
+
+  println("# run all containing *")
+  run(_.name contains "*")
+}
