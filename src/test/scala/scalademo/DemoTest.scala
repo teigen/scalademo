ddf7ea9b3e501f5f287c8b630aa9087abdd21746
Jon-Anders <mail@jteigen.com>
2009-02-15 21:43:19 +0100

    typo

--- src/test/scala/scalademo/DemoTest.scala
+++ src/test/scala/scalademo/DemoTest.scala
@@ -1,6 +1,6 @@
 package scalademo
 
-/* Copyright [2009] [Jon-Anders Teige]
+/* Copyright [2009] [Jon-Anders Teigen]
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
